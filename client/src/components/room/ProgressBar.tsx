08a266e81323fd2660af4691acd8ab6fc0c3f77c
Athul Anoop <athulanoop2020@gmail.com>
2024-11-02 23:29:03 +0530

    fix

--- client/src/components/room/ProgressBar.tsx
+++ client/src/components/room/ProgressBar.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { AlertCircle, Clock, Fingerprint, Server } from "lucide-react";
 import { T_Pigeon } from "@store/pigeonStore";
 
