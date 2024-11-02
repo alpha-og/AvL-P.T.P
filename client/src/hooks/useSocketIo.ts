96595647f8d4eaa5ab8a134932deb8c429b1d93d
Athul Anoop <athulanoop2020@gmail.com>
2024-11-02 15:49:13 +0530

    update: ensure socket connection does not exist before instantiating socket

--- client/src/hooks/useSocketIo.ts
+++ client/src/hooks/useSocketIo.ts
@@ -1,9 +1,30 @@
-import { io } from "socket.io-client";
+import { io, Socket } from "socket.io-client";
+import { useEffect, useRef } from "react";
+
 const URL = import.meta.env.PROD ? undefined : "http://localhost:3210";
 
-export const useSocketIo = () => {
-  const socket = io(URL, {
-    autoConnect: false,
-  });
-  return socket;
-};
+export default function useSocketIo() {
+  const socketRef = useRef<Socket | null>(null);
+
+  // Initialize the socket only once
+  if (!socketRef.current) {
+    socketRef.current = io(URL, {
+      reconnection: true,
+    });
+  }
+
+  useEffect(() => {
+    const socket = socketRef.current;
+    if (!socket) return;
+
+    // Manually connect the socket
+    socket.connect();
+
+    // Cleanup on unmount
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
+
+  return socketRef.current;
+}
